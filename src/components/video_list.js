36b4dfe26b5b3ca6b2879816b124559e4a2115cc
jiachen <380188469@qq.com>
2017-08-29 21:55:11 +1000

    components draft

---
+++ src/components/video_list.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+//why functional component instead of class-based component? the VideoList could
+//change?
+//refactor from function-based component -> class-based componet: props -> this.props
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video}/>
+  });
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 //import Youtube search API
 import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD71tdbPviplyO4OLqFJmg84_2CR7lzuvM';
 
-const App = () => {
-  return <div>Hi</div>;
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {videos:[]};
+    //use youtube search api to get videos and update the state's videos array
+    YTSearch({key: API_KEY, term:'surfboards'},(videos) => {
+      this.setState({videos});
+      //this.setState({videos:videos});
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <SearchBar/>
+        {/*App pass videos data to VideoList via props*/}
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
